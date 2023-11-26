e2aa9c0c380793e619b50b3e52c709f903cff083
Modem Nithesh <modnithesh@gmail.com>
2023-11-26 17:38:02 +0530

    Refactored imports

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,6 +1,5 @@
-import IssueStatusBadge from "@/app/components/IssueStatusBadge";
-import { Heading, Flex, Card, Box } from "@radix-ui/themes";
-import Skeleton from "react-loading-skeleton";
+import { Flex, Card, Box } from "@radix-ui/themes";
+import { Skeleton } from "@/app/components";
 import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingIssueDetailPage = () => {
