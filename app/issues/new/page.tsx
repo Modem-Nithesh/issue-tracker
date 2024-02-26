bf3bebb10d9bbc3c2581be6984a7714c66526b0b
Modem Nithesh <modnithesh@gmail.com>
2024-02-26 12:55:43 +0530

    added markdown box

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,20 +1,21 @@
-'use client'
-import { Button, TextArea, TextField } from '@radix-ui/themes'
-import React from 'react'
+"use client";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import { Button, TextField } from "@radix-ui/themes";
+import React from "react";
 
 const NewIssuePage = () => {
   return (
-    <div className='max-w-xl space-y-3'>
-        <TextField.Root>
-            <TextField.Input placeholder='Title' />   
-        </TextField.Root>
+    <div className="max-w-xl space-y-3">
+      <TextField.Root>
+        <TextField.Input placeholder="Title" />
+      </TextField.Root>
 
+      <SimpleMDE placeholder="description" />
 
-        <TextArea  placeholder='description'/>
-
-        <Button>Submit new issue</Button>
+      <Button>Submit new issue</Button>
     </div>
-  )
-}
+  );
+};
 
-export default NewIssuePage
+export default NewIssuePage;
