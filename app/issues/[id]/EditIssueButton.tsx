4624bf540e8e045b83d85e357095f01b784d3544
Modem Nithesh <modnithesh@gmail.com>
2024-03-02 17:46:05 +0530

    Refactor: applied Single Responsibility principle

---
+++ app/issues/[id]/EditIssueButton.tsx
@@ -0,0 +1,19 @@
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
+import React from "react";
+
+const EditIssueButton = ({issueId}: {issueId: number}) => {
+  return (
+    <div>
+   
+      <Button>
+        <Pencil2Icon />
+        <Link href={`/issues/${issueId}/edit`}></Link>
+        Edit Issue
+      </Button>
+    </div>
+  );
+};
+
+export default EditIssueButton;
