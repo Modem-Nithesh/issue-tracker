da97a38427c31a887a11563c7ab63e0039f4170a
Modem Nithesh <modnithesh@gmail.com>
2024-03-02 13:06:08 +0530

    Refactor: Organizing imports

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,18 +1,14 @@
-import { Button, Table } from "@radix-ui/themes";
-import Link from "../components/Link";
-import React from "react";
+import { IssueStatusBadge, Link } from "@/app/components";
+import { Table } from "@radix-ui/themes";
 import prisma from "@/prisma/client";
-import IssueStatusBadge from "../components/IssueStatusBadge";
-import delay from "delay";
 import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
-  await delay(2000);
+
 
   return (
     <div>
-     
       <IssueActions />
 
       <Table.Root variant="surface">
@@ -33,11 +29,13 @@ const IssuesPage = async () => {
             <Table.Row key={issue.id}>
               <Table.Cell>
                 <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
-            
-                <div className="block md:hidden"><IssueStatusBadge status={issue.status}/></div>
+
+                <div className="block md:hidden">
+                  <IssueStatusBadge status={issue.status} />
+                </div>
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
-                <IssueStatusBadge status={issue.status}/>
+                <IssueStatusBadge status={issue.status} />
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
                 {issue.createdAt.toDateString()}
