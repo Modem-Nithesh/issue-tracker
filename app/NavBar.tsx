91e321439e03236ec1349c22e82c70507c9a2134
Modem Nithesh <modnithesh@gmail.com>
2024-03-07 12:34:50 +0530

    Remove duplicate skeletons

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,34 +1,43 @@
-'use client';
+"use client";
 
-import Link from "next/link"
+import Link from "next/link";
 import classNames from "classnames";
 import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
 
-
 const NavBar = () => {
+  const currentPath = usePathname();
+  console.log(currentPath);
 
-    const currentPath = usePathname();
-    console.log(currentPath)
-
- const links = [
-    { label: "Dashboard", href: '/'},
-    { label: "Issues", href: '/issues'},
-
- ]
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues/list" },
+  ];
 
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-        <Link href='/'><AiFillBug /></Link>
-        <ul className="flex  space-x-6">
-           {links.map(link =>  <li><Link key={link.href} href={link.href} className={classNames({
-            'text-zinc-900': link.href === currentPath,
-            'text-zinc-500': link.href !== currentPath,
-            'hover:text-zinc-800 transition-colors': true
-           })}>{link.label}</Link></li>)}
-        </ul>
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex  space-x-6">
+        {links.map((link) => (
+          <li>
+            <Link
+              key={link.href}
+              href={link.href}
+              className={classNames({
+                "text-zinc-900": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -32,7 +32,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
     try {
       if (issue) await axios.patch("/api/issues/" + issue.id, data);
       else await axios.post("/api/issues", data);
-      router.push("/issues");
+      router.push("/issues/list");
       router.refresh();
       setIsSubmitting(true);
     } catch (error) {
