52c99fab1286623839a15635d96a89a503f0f1ba
Modem Nithesh <modnithesh@gmail.com>
2024-03-07 12:03:35 +0530

    improve user experience

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,6 +1,7 @@
 import { issueSchema } from "@/app/validationSchema";
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
+import delay from "delay";
 
 
 export async function PATCH( request: NextRequest,  {params}: { params: {id: string}}) {
@@ -35,10 +36,14 @@ export async function PATCH( request: NextRequest,  {params}: { params: {id: str
 export async function DELETE(
     request: NextRequest, 
     { params }: { params: { id: string }}) {
+
+      
   
     const issue = await prisma.issue.findUnique({
       where: { id: parseInt(params.id) }
     });
+
+ 
   
     if (!issue)
       return NextResponse.json({ error: 'Invalid issue'}, { status: 404 });
